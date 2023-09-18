90b44a355f4e1e39b5fc746b5a03f7d0d942e4e5
Juan Quez <juanquez.dev@gmail.com>
2023-09-18 16:59:19 -0500

    v 2.1.5

--- bin/cli.js
+++ bin/cli.js
@@ -17,44 +17,46 @@ const gitCheckoutCommand = `git clone --depth 1 https://github.com/JuanQuez/crea
 const installDepsCommand = `cd ${repoName} && npm install`;
 
 console.log(
-    "                                                  \n"+
-    "    \x1b[1m\x1b[33m\x1b[40m INSTALLING YOUR PROJECT WITH NAME \x1b[0m\n"+
-    `                \x1b[1m\x1b[32m${repoName}\x1b[0m               \n`+
-    "                                         \n"+    
-    "  █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█\n"+
-    "  █ \x1b[33m██ ██ ██ ██ ██ ██ ██ ██ ██\x1b[0m          █\n"+
-    "  █ \x1b[33m██ ██ ██ ██ ██ ██ ██ ██ ██\x1b[0m          █\n"+
-    "  █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█\n"+
-    "                                         \n"+
-    "    █    █▀▀▀█ █▀▀█ █▀▀▄ ▀█▀ █▄  █ █▀▀█  \n"+
-    "    █    █   █ █▄▄█ █  █  █  █ █ █ █ ▄▄  \n"+
-    "    █▄▄█ █▄▄▄█ █  █ █▄▄▀ ▄█▄ █  ▀█ █▄▄█  \n"+
+    "                                                  \n" +
+    "    \x1b[1m\x1b[33m\x1b[40m INSTALLING YOUR PROJECT WITH NAME \x1b[0m\n" +
+    `                \x1b[1m\x1b[32m${repoName}\x1b[0m               \n` +
+    "                                         \n" +
+    "  █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█\n" +
+    "  █ \x1b[33m██ ██ ██ ██ ██ ██ ██ ██ ██\x1b[0m          █\n" +
+    "  █ \x1b[33m██ ██ ██ ██ ██ ██ ██ ██ ██\x1b[0m          █\n" +
+    "  █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█\n" +
+    "                                         \n" +
+    "    █    █▀▀▀█ █▀▀█ █▀▀▄ ▀█▀ █▄  █ █▀▀█  \n" +
+    "    █    █   █ █▄▄█ █  █  █  █ █ █ █ ▄▄  \n" +
+    "    █▄▄█ █▄▄▄█ █  █ █▄▄▀ ▄█▄ █  ▀█ █▄▄█  \n" +
     "                                         \n"
-    );
+);
 const checkedOut = runCommand(gitCheckoutCommand);
 if (!checkedOut) process.exit(-1);
 
 console.log(
-    "                                               \n"+
-    "         \x1b[1m\x1b[36m\x1b[40m INSTALLING DEPENDENCIES \x1b[0m\n"+
+    "                                               \n" +
+    "         \x1b[1m\x1b[36m\x1b[40m INSTALLING DEPENDENCIES \x1b[0m\n" +
     "                                                 "
-    );
+);
 const installedDeps = runCommand(installDepsCommand);
 if (!installedDeps) process.exit(-1);
 
 console.log(
-    "          \x1b[1m\x1b[32m ¡Tu creación está lista! \x1b[0m\n"+
-    ` Ingresa \x1b[1m\x1b[32m "cd ${repoName}" \x1b[0m y despues "code ."\n`+
+    "                                         \n" +
+    "  █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█\n" +
+    "  █\x1b[32m ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ \x1b[0m█\n" +
+    "  █\x1b[32m ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ \x1b[0m█\n" +
+    "  █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█\n" +
+    "                                         \n" +
+    "               █▀▀█ █▀▀▀█ █              \n" +
+    "               █ ▄▄ █   █ █              \n" +
+    "               █▄▄█ █▄▄▄█ ▄              \n" +
+    "                                         \n"
+)
+console.log(
+    "          \x1b[1m\x1b[32m ¡Tu creación está lista! \x1b[0m\n" +
+    ` Ingresa \x1b[1m\x1b[32m "cd ${repoName}" \x1b[0m y despues "code ."\n` +
     `No olvides crear tu base de datos y agregarla\n` +
-    `           En el archivo \x1b[1m\x1b[32m ".env" \x1b[0m\n`+
-"                                         \n"+    
-"  █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█\n"+
-"  █\x1b[32m ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ \x1b[0m█\n"+
-"  █\x1b[32m ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ \x1b[0m█\n"+
-"  █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█\n"+
-"                                         \n"+
-"               █▀▀█ █▀▀▀█ █              \n"+
-"               █ ▄▄ █   █ █              \n"+
-"               █▄▄█ █▄▄▄█ ▄              \n"+
-"                                         \n"
-);
+    `           En el archivo \x1b[1m\x1b[32m ".env" \x1b[0m\n`
+)
